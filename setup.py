05537f6b98dd8738682e6a9a1be9ff8d2e1a7f14
thethiago27 <thethiago27@gmail.com>
2024-03-17 20:40:51 -0400

    init: project

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='lambda_aws_creator',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         'click',
     ],
     entry_points='''
         [console_scripts]
-        generate-lambda-routes=cli:generate_lambda_routes
+        generate-lambda-routes=lambda_decorators.cli:generate_lambda_routes
     '''
 )
